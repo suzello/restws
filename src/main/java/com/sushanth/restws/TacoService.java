6304bb7bbf27283c1f16b74bd1725ed9eb27df38
suzello <sushanthpatwari@gmail.com>
2018-09-08 21:53:22 -0400

    changes to revert back to old code

--- src/main/java/com/sushanth/restws/TacoService.java
+++ src/main/java/com/sushanth/restws/TacoService.java
@@ -2,16 +2,27 @@ package com.sushanth.restws;
 
 import java.util.List;
 
-import com.sushanth.restws.model.OrderTotal;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
 import com.sushanth.restws.model.Tacos;
 
-
+@Consumes("application/xml,application/json")
+@Produces("application/xml,application/json")
+@Path("/tacoservice")
 public interface TacoService {
 
-	
-	public List<Tacos> getTacos();
+	@Path("/tacos")
+	@GET
+	List<Tacos> getTacos();
 
-	
-	public OrderTotal createOrder(List<Tacos> tacoOrder);
+	@Path("/order")
+	@POST
+	Response createOrder(List<Tacos> tacoOrder);
 
 }
