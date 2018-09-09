c93db047a1c300c9cd579c2422435ee20c0d9c9b
suzello <sushanthpatwari@gmail.com>
2018-09-08 21:55:10 -0400

    changes to revert back to old code

--- src/main/java/com/sushanth/restws/model/Tacos.java
+++ src/main/java/com/sushanth/restws/model/Tacos.java
@@ -4,22 +4,13 @@ import java.util.List;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
-
+@XmlRootElement(name = "Tacos")
 public class Tacos {
 
 	private Double price;
 	private String item;
 	private Integer quantity;
 
-	public Tacos(Double price, String item, Integer quantity) {
-		super();
-		this.price = price;
-		this.item = item;
-		this.quantity = quantity;
-	}
-	public Tacos(){
-		
-	}
 	// price,name,quantity of each taco
 	public Double getPrice() {
 		return price;
@@ -43,13 +34,5 @@ public class Tacos {
 
 	public void setQuantity(Integer quantity) {
 		this.quantity = quantity;
-	}
-	@Override
-	public String toString() {
-		return "Tacos [price=" + price + ", item=" + item + ", quantity=" + quantity + "]";
 	}	
-	@Override
-	public boolean equals(Object o){
-		return this.toString().equalsIgnoreCase(o.toString());
-	}
 }
