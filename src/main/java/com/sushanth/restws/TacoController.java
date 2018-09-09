455ae20e03a3f3aae66448c8254e1a9ad4f7b952
suzello <sushanthpatwari@gmail.com>
2018-09-08 21:53:36 -0400

    changes to revert back to old code

--- src/main/java/com/sushanth/restws/TacoController.java
+++ src/main/java/com/sushanth/restws/TacoController.java
@@ -1,30 +1,5 @@
 package com.sushanth.restws;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.sushanth.restws.model.OrderTotal;
-import com.sushanth.restws.model.Tacos;
-
-@RestController
-@RequestMapping(value="/path")
 public class TacoController {
 
-	@Autowired
-	private TacoService tacoService;
-	
-	@RequestMapping(value="/tacos",method=RequestMethod.GET)
-	public List<Tacos> getTacos(){
-		return tacoService.getTacos();
-	}
-	
-	@RequestMapping(value="/order",method=RequestMethod.POST)
-	public OrderTotal postTacos(@RequestBody List<Tacos> tacoOrder){
-		return tacoService.createOrder(tacoOrder);
-	}
 }
